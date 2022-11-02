92385780ceeeac7e0ad5db0685b72c2c726a2f92
Khatanboyev Elbek <programmerelbek@gmail.com>
2022-11-02 21:04:51 +0500

    Leetcode: Running of 1d Array solved

--- RunningSum/Program.cs
+++ RunningSum/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace RunningSum
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] example = { 4, 2, 6, 5 };
+            foreach(int i in Result(example))
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static int[] Result(int[] nums)
+        {
+            for(int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;
         }
     }
 }
