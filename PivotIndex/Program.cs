17edb857c8ea91a619f703ae9880e81db6a1d0f3
Khatanboyev Elbek <programmerelbek@gmail.com>
2022-11-03 10:31:07 +0500

    Leetcode: Find pivot index solved

---
+++ PivotIndex/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PivotIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] example = { 1, 2, 3 };
+            int result = Result(example);
+            Console.WriteLine(result);
+        }
+        public static int Result(int[] nums)
+        {
+            int totalSum = 0;
+            int leftSum = 0;
+            foreach (int num in nums)
+            {
+                totalSum += num;
+            }
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(totalSum - leftSum - nums[i] == leftSum)
+                {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+            return -1;
+        }
+    }
+}
