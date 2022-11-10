314f9d33daf2ef32c3ad33e5367c604d48a2e5c4
khatanboyevelbek <programmerelbek@gmail.com>
2022-11-10 22:57:11 +0500

    Leetcode: Palindrome Number solved

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IsPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Result: {IsPalindrome(121)}");
+        }
+        public static bool IsPalindrome(int x)
+        {
+            int reverseNumber = 0;
+            int num = x;
+            //if (x <= 9)
+            //{
+            //    return x;
+            //}
+            while (x >= 1)
+            {
+                reverseNumber = reverseNumber * 10 + x % 10;
+                x = x / 10;
+            }
+            return reverseNumber == num;
+        }
+    }
+}
